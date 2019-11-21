4cd0493c9126f9af8a9f8a6d3fd81488c6b96551
Chris van Halewyn <cvh@pay2escrow.co.uk>
2019-11-21 16:09:34 +0000

    End of day commit from Work Computer

--- TF03_module.py
+++ TF03_module.py
@@ -24,3 +24,20 @@ class TF_03:
                 self.ptr = 1
                 if ch == self.frame_marker:  #this is the special case of a lidar range frame of 9 bytes (7 left)
                     num_to_read = 7
+                else:
+                    num_to_read = ch  # todo this needs a range check in case of corruption
+            else:
+                self.buf[self.ptr] = ch
+                self.ptr += 1
+
+        #this point is reached if there is nothing left to read without a frame being detected,
+        # a frame has been collected, or a frame is partly collected
+        if num_to_read == ptr: #this means a complete frame has been read
+            #checksum check goes here
+            self.ptr = -1
+            return True
+        else:
+            #either ptr still is -1 because nothing has been read or ptr is a value from which reading can continue
+            return False
+
+        #todo need to track this through execution and notify when buf is complete after checksum check
